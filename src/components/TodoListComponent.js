f98f990f73cfca2ac99af339e31c32470780e345
carbonbes <pawel-iwanow@mail.ru>
2021-02-23 14:43:07 +0300

    небольшие изменения

--- src/components/TodoListComponent.js
+++ src/components/TodoListComponent.js
@@ -40,8 +40,8 @@ const TodoListComponent = () => {
               </thead>
                 {todos
                   .filter((todo) => todo.title
-                  .includes(searchingFilter.toLowerCase()) && todo.completed.toString()
-                  .includes(completedFilter.toString()))
+                  .includes(searchingFilter.toLowerCase()) && todo.completed
+                  .includes(completedFilter))
                   .map((todo) => <TodoComponent key={todo.id} todo={todo} />)}
             </table>
           </div>
