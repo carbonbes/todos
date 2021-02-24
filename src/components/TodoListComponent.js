4291e22236177368b1f3600e945ac4f3ec5783d6
carbonbes <pawel-iwanow@mail.ru>
2021-02-24 23:04:02 +0300

    Minor fixes

--- src/components/TodoListComponent.js
+++ src/components/TodoListComponent.js
@@ -4,7 +4,6 @@ import { requestTodo } from "../actions/todoActions";
 import SortingTodosComponent from "./SortingTodosComponent";
 import SearchTodosComponent from "./SearchTodosComponent";
 import TodoComponent from "./TodoComponent";
-import TodoEmptyAlertComponent from "./TodoEmptyAlertComponent";
 
 const TodoListComponent = () => {
   const dispatch = useDispatch();
@@ -15,41 +14,41 @@ const TodoListComponent = () => {
 
   const todos = useSelector((state) => state.todos);
   const searchingFilter = useSelector((state) => state.searchFilter);
-  const completedFilter = useSelector((state) => state.completedFilter.toString());
+  const completedFilter = useSelector((state) =>
+    state.completedFilter.toString()
+  );
 
   return (
-    todos.length ? (
-      <>
-        <div className="row p-3 px-0 pb-0 bg-white sticky-top shadow-sm">
-          <h1 className='fw-bold'>Мои задачи</h1>
-          <div className="col pt-2">
-            <SortingTodosComponent />
-          </div>
-          <div className="col pt-2">
-            <SearchTodosComponent />
-          </div>
+    <>
+      <div className="row p-3 px-0 pb-0 bg-white sticky-top shadow-sm">
+        <h1 className="fw-bold">Мои задачи</h1>
+        <div className="col pt-2">
+          <SortingTodosComponent />
         </div>
-        <div className="row pt-2 bg-white">
-          <div className="col">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Название</th>
-                </tr>
-              </thead>
-                {todos
-                  .filter((todo) => todo.title
-                  .includes(searchingFilter.toLowerCase()) && todo.completed.toString()
-                  .includes(completedFilter))
-                  .map((todo) => <TodoComponent key={todo.id} todo={todo} />)}
-            </table>
-          </div>
+        <div className="col pt-2">
+          <SearchTodosComponent />
         </div>
-      </>
-    ) : (
-      <TodoEmptyAlertComponent />
-    )
+      </div>
+      <div className="row pt-2 bg-white">
+        <div className="col">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Название</th>
+              </tr>
+            </thead>
+            {todos
+              .filter((todo) =>
+                  todo.title.includes(searchingFilter.toLowerCase()) &&
+                  todo.completed.toString().includes(completedFilter)
+              ).map((todo) => (
+                <TodoComponent key={todo.id} todo={todo} />
+              ))}
+          </table>
+        </div>
+      </div>
+    </>
   );
 };
 
